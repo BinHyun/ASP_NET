bb8e04b007a2524f32d091f555a5c43777252918
BinHyun <binhk1004@gmail.com>
2025-01-28 01:45:06 +0900

    First Commit

---
+++ Application/Application/Controllers/BoardController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Application.Controllers
+{
+    public class BoardController : Controller
+    {
+        // GET: Board
+        //매개변수 설정 시, 데이터 타입 뒤에 ? 를 붙여주면 null 허용
+        public string List(int? id)
+        {
+            if (id == null)
+            {
+                return "Error Message #1";
+            } else
+            {
+                return "Board id : " + id.Value;
+            }
+            
+        }
+    }
+}
